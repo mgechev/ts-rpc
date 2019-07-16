0f69fa25ea6816f86829479642753d63e4d0e73f
mgechev <mgechev@gmail.com>
2019-07-15 20:45:43 -0700

    Move server to async functions

--- example/angular/server/index.ts
+++ example/angular/server/index.ts
@@ -5,27 +5,22 @@ import { TodoOrm } from './db';
 
 @Service()
 class TodosService implements TodosService1 {
-  getAll(): Promise<Todo[]> {
-    return Promise.resolve(TodoOrm.findAll()).then(todos =>
-      todos.sort((a: Todo, b: Todo) => parseInt(a.id) - parseInt(b.id))
-    );
+  async getAll(): Promise<Todo[]> {
+    const todos = await Promise.resolve(TodoOrm.findAll());
+    return todos.sort((a: Todo, b: Todo) => parseInt(a.id) - parseInt(b.id));
   }
 
-  createTodo(todo: Todo): Promise<Todo> {
-    return Promise.resolve(TodoOrm.create(todo)).then(({ id }: { id: string }) => {
-      todo.id = id;
-      return todo;
-    });
+  async createTodo(todo: Todo): Promise<Todo> {
+    const { id } = await Promise.resolve(TodoOrm.create(todo));
+    todo.id = id;
+    return todo;
   }
 
-  updateTodo(todo: Todo): Promise<Todo> {
-    return Promise.resolve(
-      TodoOrm.update(todo, {
-        where: { id: todo.id }
-      })
-    ).then(_ => {
-      return todo;
-    });
+  async updateTodo(todo: Todo): Promise<Todo> {
+    const _ = await Promise.resolve(TodoOrm.update(todo, {
+      where: { id: todo.id }
+    }));
+    return todo;
   }
 
   deleteTodo({ id }: { id: string }): Promise<void> {
