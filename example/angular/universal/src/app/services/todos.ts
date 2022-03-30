8f6aef557a148815153b63831492bda2af3c8b67
mgechev <mgechev@gmail.com>
2022-03-30 09:24:00 -0700

    Update style and package-lock.json

--- example/angular/universal/src/app/services/todos.ts
+++ example/angular/universal/src/app/services/todos.ts
@@ -2,7 +2,7 @@ import { Todo } from '../models/todo';
 import { Read, RequiresAuth, rpc, serviceFactory } from 'ts-rpc-reflect';
 import { TransferState } from 'ts-rpc-angular';
 import { Injectable } from '@angular/core';
-import {StaticInMemoryTokenHandler} from 'ts-rpc-lib';
+import { StaticInMemoryTokenHandler } from 'ts-rpc-lib';
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +12,10 @@ export abstract class TodosService {
   @RequiresAuth(StaticInMemoryTokenHandler)
   @TransferState() @Read() getAll(): Promise<Todo[]> { return rpc(); }
   createTodo(todo: Partial<Todo>): Promise<Todo> { return rpc(); }
+  
   @RequiresAuth(StaticInMemoryTokenHandler)
   updateTodo(todo: Todo): Promise<Todo> { return rpc(); }
+
   @RequiresAuth(StaticInMemoryTokenHandler)
   deleteTodo({ id }: { id: string }): Promise<void> { return rpc(); }
 }
